c1696dd507c6570d055c91af36a312e7fa735960
jccjames422 <insitefuldesign@gmail.com>
2022-11-13 15:42:45 -0500

    Reorganized Controllers, updated method names, cleaned up html, removed unused javascript and css

--- InternTest/Controllers/HomeController.cs
+++ InternTest/Controllers/HomeController.cs
@@ -19,50 +19,6 @@ namespace InternTest.Controllers
             return View(viewModel);
         }
 
-        [HttpPost]
-        public ActionResult DeleteStudent(int id)
-        {
-            int numberRemoved = FauxStudentDb.students.RemoveAll(student => student.Id == id);
-            return Json(new {studentsRemoved = numberRemoved});
-        }
-
-        public ActionResult AddStudent()
-        {
-            return View("AddStudent");
-        }
-
-
-        [HttpPost]
-        public ActionResult AddStudent([Bind(Include = "firstName,lastName,field,age")] StudentModel student)
-        {
-            if (ModelState.IsValid)
-            {
-                student.Id = FauxStudentDb.incrementID++;
-                FauxStudentDb.students.Add(student);
-                return RedirectToAction("Index", "Home", new { area = "", });
-            } else
-            {
-                return View("AddStudent");
-            }
-        }
-
-        //public ActionResult EditUser(string first, string last, string field, int age)
-        //{
-        //    StudentModel student = new StudentModel(first, last, field, age);
-        //    for (int i = FauxStudentDb.students.Count() - 1; i >= 0; i--)
-        //    {
-        //        if (FauxStudentDb.students[i].Equals(student))
-        //        {
-        //            FauxStudentDb.students[i].FirstName = first;
-        //            FauxStudentDb.students[i].LastName = last;
-        //            FauxStudentDb.students[i].Field = field;
-        //            FauxStudentDb.students[i].Age = age;
-        //        }
-        //    }
-        //    return Index();
-        //}
-
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ InternTest/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using InternTest.Models;
+using InternTest.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InternTest.Controllers
+{
+    public class StudentController : Controller
+    {
+         public ActionResult Create()
+        {
+            return View("Edit");
+        }
+
+        public ActionResult Edit(StudentModel student)
+        {
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public ActionResult Update([Bind(Include = "firstName,lastName,field,age")] StudentModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = FauxStudentDb.incrementID++;
+                FauxStudentDb.students.Add(student);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View("Edit");
+        }
+
+        [HttpPost]
+        public ActionResult Destroy(int id)
+        {
+            int numberRemoved = FauxStudentDb.students.RemoveAll(student => student.Id == id);
+            return Json(new { studentsRemoved = numberRemoved });
+        }
+    }
+}
