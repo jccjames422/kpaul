6606d91a9c0da38b2cfa5132c6bc40075426ac65
jccjames422 <insitefuldesign@gmail.com>
2022-11-13 20:31:50 -0500

    renamed method names to better conform to the seven restful actions.  Styled the edit and delete buttons in the Home/Index view.

--- InternTest/Controllers/HomeController.cs
+++ InternTest/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using InternTest.Models;
-using InternTest.Utilities;
+﻿using InternTest.Utilities;
 using InternTest.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InternTest.Controllers
--- InternTest/Controllers/StudentController.cs
+++ InternTest/Controllers/StudentController.cs
@@ -1,33 +1,44 @@
 ﻿using InternTest.Models;
 using InternTest.Utilities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace InternTest.Controllers
 {
     public class StudentController : Controller
     {
-         public ActionResult Create()
+         public ActionResult New()
         {
-            return View("Edit");
+            return View();
         }
-
-        public ActionResult Edit(StudentModel student)
-        {
-            return View(student);
-        }
-
 
         [HttpPost]
-        public ActionResult Update([Bind(Include = "firstName,lastName,field,age")] StudentModel student)
+        public ActionResult Create([Bind(Include = "firstName,lastName,field,age")] StudentModel student)
         {
             if (ModelState.IsValid)
             {
                 student.Id = FauxStudentDb.incrementID++;
                 FauxStudentDb.students.Add(student);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View("Edit");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var student = FauxStudentDb.students.Where(s => s.Id == id).First();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Update([Bind(Include = "id,firstName,lastName,field,age")] StudentModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                FauxStudentDb.students.Remove(student); // remove the old record
+                FauxStudentDb.students.Add(student); // add the updated record
+                FauxStudentDb.students = FauxStudentDb.students.OrderBy(s => s.Id).ToList(); // sort the list by Id to preserve the order
                 return RedirectToAction("Index", "Home");
             }
 
